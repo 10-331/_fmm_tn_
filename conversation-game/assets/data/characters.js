7ab27b77ad5d16b66eea52e8446b39cd9e3d91b2
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-03-28 19:37:42 +0900

    Update characters.js

--- conversation-game/assets/data/characters.js
+++ conversation-game/assets/data/characters.js
@@ -3,27 +3,6 @@ const characters = [
     id: "child",
     label: "⬛︎",
     status: "coming_soon",
-    statusLabel: "準備中"
-  },
-  {
-    id: "filler",
-    label: "フィラー",
-    status: "playable",
-    statusLabel: "会話可能"
-  },
-  {
-    id: "aya",
-    label: "綾",
-    status: "coming_soon",
-    statusLabel: "準備中"
-  }
-];
-
-const characters = [
-  {
-    id: "child",
-    label: "⬛︎",
-    status: "coming_soon",
     statusLabel: "準備中",
     image: "./assets/img/child-bust.png"
   },
